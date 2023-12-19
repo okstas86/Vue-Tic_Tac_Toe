ebcc9ec0b5efd9b43e2b44879cae5a6f533fd932
okstas86 <stanislavocunev1@gmail.com>
2023-12-19 17:16:19 +0200

    creating a component GameBoard and adding the ability to select a square

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,11 @@
 			<Player name="Player 1" symbol="X" />
 			<Player name="Player 2" symbol="O" />
 		</ol>
+		<GameBoard />
 	</main>
 </template>
 
 <script setup>
 import Player from './components/Player.vue'
+import GameBoard from './components/GameBoard.vue'
 </script>
---
+++ src/components/GameBoard.vue
@@ -0,0 +1,29 @@
+<template>
+	<ol id="game-board">
+		<li v-for="(row, rowIndex) in initialGameboard" :key="rowIndex">
+			<ol>
+				<li v-for="(playerSymbol, colIndex) in row" :key="colIndex">
+					<button @click="() => handleSelectSquere(rowIndex, colIndex)">
+						{{ playerSymbol }}
+					</button>
+				</li>
+			</ol>
+		</li>
+	</ol>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const initialGameboard = ref([
+	[null, null, null],
+	[null, null, null],
+	[null, null, null],
+])
+
+const gameBoard = ref(initialGameboard)
+
+function handleSelectSquere(rowIndex, colIndex) {
+	gameBoard.value[rowIndex][colIndex] = 'X'
+}
+</script>
