190ed5b15ed4cf5ab19a60f9c3c142fc9170ca40
okstas86 <stanislavocunev1@gmail.com>
2023-12-19 10:23:27 +0200

    Create component Player

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
-<script setup></script>
-
 <template>
-	<div></div>
+	<main id="game-container">
+		<ol id="players">
+			<Player name="Player 1" symbol="X" />
+			<Player name="Player 2" symbol="O" />
+		</ol>
+	</main>
 </template>
+
+<script setup>
+import Player from './components/Player.vue'
+</script>
---
+++ src/components/Player.vue
@@ -0,0 +1,32 @@
+<template>
+	<li>
+		<span class="player">
+			<span v-if="!isEditing" class="player-name">{{ playerName }}</span>
+			<input v-else type="text" required v-model="playerName" />
+			<span class="player-symbol">{{ symbol }}</span>
+		</span>
+		<button @click="handleEditClick">{{ isEditing ? 'Save' : 'Edit' }}</button>
+	</li>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+	name: {
+		type: String,
+		requared: true,
+	},
+	symbol: {
+		type: String,
+		requared: true,
+	},
+})
+
+let isEditing = ref(false)
+let playerName = props.name
+
+function handleEditClick() {
+	isEditing.value = !isEditing.value
+}
+</script>
